e379c413d8e75ed8e0277c48c8c44ff053e049a0
jabaris <130151127+jabaris@users.noreply.github.com>
2024-09-08 13:47:55 +0300

    добавил возможность озвучивать переведенные слова

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function translater() {
+let translater = () => {
     // Получаем значение из текстового поля
     var sourceWord = document.getElementById('sourceWord').value;
 
@@ -15,6 +15,37 @@ function translater() {
         },
         error: function(xhr, status, error) {
             console.error('Ошибка: ', error);
+        }
+    });
+}
+
+let speak = () => {
+    var translatedText = document.getElementById('sourceWord').value;
+    if (!translatedText) {
+        alert('Нет переведенного слова для озвучивания.');
+        return;
+    }
+
+    // Попытка воспроизведения в нескольких форматах
+    var audioFormats = ['m4a', 'mp3', 'ogg']; // Указываем все поддерживаемые форматы
+    var audioPlayed = false;
+
+    audioFormats.forEach(function(format) {
+        if (!audioPlayed) {
+            var audioFileName = translatedText.toLowerCase().replace(/\s+/g, '_') + '.' + format;
+            var audioFilePath = 'audio/' + audioFileName;
+
+            var audio = new Audio(audioFilePath);
+            audio.onerror = function() {
+                console.warn('Аудиофайл не найден: ' + audioFilePath);
+            };
+            audio.oncanplaythrough = () => {
+                if (!audioPlayed) {
+                    audioPlayed = true;
+                    audio.play();
+                }
+            };
+            audio.load();
         }
     });
 }
